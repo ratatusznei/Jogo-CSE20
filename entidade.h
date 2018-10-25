8ba40e100b66377fbee5744f02f5fe2cebcd218f
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-10-25 17:49:01 -0300

    Update entidade.h

--- entidade.h
+++ entidade.h
@@ -8,22 +8,31 @@ protected:
     int py;     //Posição em y da entidade
     int width;  //Largura da hitbox da entidade
     int height; //Altura da hitbox da entidade
+    int flag;   /*Determina internamente como a entidade vai se comportar
+                  Flag negativa = inimigo
+                  Flag positiva = amigo
+                  Flag zero = plataforma ou obstáculo
+                  Basicamente é uma maneira da entidade saber o que ela é
+                */
     
 public:
 
     Entidade();
     ~Entidade();
     
-    void setpx(int x);
-    int getpx();
+    void setx(int x);
+    int getx();
     
-    void setpy(int y);
-    int getpy();
+    void sety(int y);
+    int gety();
     
     void setwidth(int w);
     int getwidth();
     
     void setheight(int h);
     int getheight();
+    
+    void setflag(int f);
+    int getflag();
     
 };
