e3039a9aa5f60451ee8c7afefa246c1134cee486
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-10-25 18:04:02 -0300

    Create entidade.cc

---
+++ entidade.cc
@@ -0,0 +1,53 @@
+#include"entidade.h"
+
+Entidade::Entidade(){
+  
+    px = 0;
+    py = 0;
+    width = 0;
+    height = 0;
+    flag = 1;
+
+}
+
+Entidade::~Entidade(){}
+
+void Entidade::setx(int x){
+    px = x;
+}
+
+int Entidade::getx(){
+    return px;
+}
+
+void Entidade::sety(int y){
+    py = y;
+}
+
+int Entidade::gety(){
+    return py;
+}
+
+void Entidade::setwidth(int w){
+    width = w;
+}
+
+int Entidade::getwidth(){
+    return width;
+}
+
+void Entidade::setheight(int h){
+    height = h;
+}
+
+int Entidade::getheight(){
+    return height;
+}
+
+void Entidade::setflag(int f){
+    flag = f;
+}
+
+int Entidade::getflag(){
+    return flag;
+}
