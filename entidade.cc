b76c2d9ed149100ae4fc524c5d674e204422b2a0
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-10-25 17:46:00 -0300

    Update entidade.cc

--- entidade.cc
+++ entidade.cc
@@ -6,16 +6,48 @@ Entidade::Entidade(){
     py = 0;
     width = 0;
     height = 0;
+    flag = 1;
 
 }
 
 Entidade::~Entidade(){}
 
-void Entidade::setpx(int px){
+void Entidade::setx(int x){
     px = x;
 }
 
-int Entidade::getpx(){
+int Entidade::getx(){
     return px;
 }
 
+void Entidade::sety(int y){
+    py = y;
+}
+
+int Entidade::gety(){
+    return py;
+}
+
+void Entidade::setwidth(int w){
+    width = w;
+}
+
+int Entidade::getwidth(){
+    return width;
+}
+
+void Entidade::setheight(int h){
+    height = h;
+}
+
+int Entidade::getheight(){
+    return height;
+}
+
+void Entidade::setflag(int f){
+    flag = f;
+}
+
+int Entidade::getflag(){
+    return flag;
+}
