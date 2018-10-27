deba70f8b79f42a3c01ccedcf552ffbff5de81d8
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-10-27 16:06:19 -0300

    Update vetor.h

--- vetor.h
+++ vetor.h
@@ -10,5 +10,57 @@ protected:
     
 public:
     
+    Vetor(T xis, T ipsilon){
+        
+        x = xis;
+        y = ipsilon;
+    
+    }
+    Vetor(){
+        
+        x = 0;
+        y = 0;
+        
+    }
+    ~Vetor(){}
+    
+    void setx(T xis){
+        x = xis;
+    }
+    T getx(){
+        return x;
+    }
+    
+    void sety(T ipsilon){
+        y = ipsilon;
+    }
+    T gety(){
+        return y;
+    }
+    
+    void operator = (Vetor& v){
+        
+        x = v.getx();
+        y = v.gety();
+        
+    }
+    void operator = (T xis, T ipsilon){
+        
+        x = xis;
+        y = ipsilon;
+        
+    }
+    Vetor operator + (Vetor& v1, Vetor& v2){
+        
+        x = v1.getx() + v2.getx();
+        y = v1.gety() + v2.gety();
+        
+    }
+    void operator += (Vetor& v){
+        
+        x = x + v.getx();
+        y = y + v.gety();
+        
+    }
 
 };
