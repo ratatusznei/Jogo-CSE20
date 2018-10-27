7102795bde53b6bd74420dbefc1811b4a927c3a8
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-10-27 16:49:22 -0300

    Update entidadedinamica.h

--- entidadedinamica.h
+++ entidadedinamica.h
@@ -6,13 +6,26 @@ class EntidadeDinamica: public Entidade{
 
 protected:
 
-    
+    Vetor<double> velocidade;
+    Vetor<double> aceleracao;
+    int direcao;
 
 public:
   
     EntidadeDinamica(int x, int y, int w, int h, int f);
     EntidadeDinamica();
     ~EntidadeDinamica();
+    
+    void setvel(Vetor<double> v);
+    void setvel(double vx, double vy);
+    Vetor<double> getvel();
+   
+    void setace(Vetor<double> a);
+    void setace(double ax, double ay);
+    Vetor<double> getace();
+    
+    void setdirecao(int d);
+    int getdirecao();
   
     virtual void executar();
 
