3509df4f5a08baf5106e5965f05ee59820591017
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-10-30 11:41:14 -0300

    Update personagem.cc

--- personagem.cc
+++ personagem.cc
@@ -1,10 +1,18 @@
 #include"personagem.h"
 
-Personagem::Personagem(int x, int y, int w, int h):
-EntidadeDinamica(x,y,w,h,1){}
+Personagem::Personagem(int x, int y, int w, int h, Vetor<double> v, Vetor<double> a, Direcao d):
+EntidadeDinamica(x,y,w,h,v,a,d){
+
+    
+
+}
 
 Personagem::Personagem():
-EntidadeDinamica(0,0,0,0,1){}
+EntidadeDinamica(0,0,0,0,Vetor<double> v(0,0), Vetor<double> a(0,0),EntityType::right){
+ 
+    
+    
+}
 
 Personagem::~Personagem(){}
 
