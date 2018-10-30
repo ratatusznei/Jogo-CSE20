e6426ff3079dd513fbd29eb6d496aa4a54834047
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-10-30 11:33:30 -0300

    Update entidadeestatica.cc

--- entidadeestatica.cc
+++ entidadeestatica.cc
@@ -1,10 +1,10 @@
 #include"entidadeestatica.h"
 
-EntidadeEstatica::EntidadeEstatica(int x, int y, int w, int h, int f):
-Entidade(x,y,w,h,f){}
+EntidadeEstatica::EntidadeEstatica(int x, int y, int w, int h):
+Entidade(x,y,w,h){}
 
 EntidadeEstatica::EntidadeEstatica():
-Entidade(0,0,0,0,0){}
+Entidade(0,0,0,0){}
 
 EntidadeEstatica::~EntidadeEstatica(){}
 
