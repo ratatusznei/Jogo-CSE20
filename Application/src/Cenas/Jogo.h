f6a2efbd070848c73aaf99dc1fa02241c770e969
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-11-17 18:57:26 -0200

    Add files via upload

--- Application/src/Cenas/Jogo.h
+++ Application/src/Cenas/Jogo.h
@@ -8,6 +8,17 @@
 #include "../GerenciadorGrafico.h"
 #include "../Fases/Fase.h"
 
+#include <SFML/System.hpp>
+#include <fstream>
+
+#include "../GerenciadorGrafico.h"
+#include "../GerenciadorDeColisao.h"
+#include "../LinkedLists.h"
+
+#include "../Jogador/Jogador.h"
+#include "../Inimigos/Mumia.h"
+#include "../Entidades/Plataforma.h"
+
 class Jogo {
 	public:
 		Jogo ();
@@ -16,8 +27,19 @@ class Jogo {
 		void Executar ();
 		void Desenhar ();
 
+		/*	IN: Path para um arquivo de fase
+		OUT: Preenche as listas de entidades conforme o arquivo */
+        void CarregarFase (char* path);
+
 	private:
-		Fase _fase;
+		Fase* _fase;
+		Jogador* _jogador1;
+        Jogador* _jogador2;
+
+        GerenciadorDeColisao colisoes;
+
+        Lista<Inimigo*> _listaInimigos;
+        Lista<Plataforma*> _listaPlataformas;
 
 };
 
