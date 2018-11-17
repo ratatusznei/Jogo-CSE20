5daa3491840063fe9dacebd50c7c7a39d63ece1f
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-11-17 15:22:41 -0200

    Add files via upload

--- Application/src/Jogador/RegistroDeJogada.cc
+++ Application/src/Jogador/RegistroDeJogada.cc
@@ -22,34 +22,30 @@ int* pontuacao, const char* fase){
     ofstream save;
     save.open(_path);
 
-    save << "F"  << endl << fase << endl << endl;
+    save << "F "  << fase << endl;
 
-    save << "J"  << endl << *pontuacao << endl;
-    save << "J1" << j1->GetPosicao().x << ' ' << j1->GetPosicao().y << endl;
-    save << "J2" << j2->GetPosicao().x << ' ' << j1->GetPosicao().y << endl;
-
-    save << endl << "I" << endl;
+    save << "J "  << *pontuacao << endl;
+    save << "K " << j1->GetPosicao().x << ' ' << j1->GetPosicao().y << endl;
+    save << "L " << j2->GetPosicao().x << ' ' << j1->GetPosicao().y << endl;
 
     do{
 
-        save << li->getWhatIsHere()->GetPosicao().x << ' '
+        save << "I " << li->getWhatIsHere()->GetPosicao().x << ' '
                 << li->getWhatIsHere()->GetPosicao().y << ' ';
         save << li->getWhatIsHere()->QualTipo() << endl;
 
     }
     while(!li--);
 
-    save << endl << "P" << endl;
-
     do{
 
-        save << lp->getWhatIsHere()->GetPosicao().x << ' '
-                << lp->getWhatIsHere()->GetPosicao().y << endl;
+        save << "P " << lp->getWhatIsHere()->GetPosicao().x << ' '
+                << lp->getWhatIsHere()->GetPosicao().y << ' '
+                 << endl;
 
     }
     while(!lp--);
 
-    free(_path);
     save.close();
 
 }
@@ -59,6 +55,71 @@ Jogador* j1, Jogador* j2,
 Lista<Inimigo*>* li, Lista<Plataforma*>* lp,
 int* pontuacao){
 
+    char* data = (char*) malloc(sizeof(char)*64);
 
+    li->mataLista();
+    lp->mataLista();
+
+    int xis, ipsilon, w, h, f;
+
+    ifstream save;
+    save.open(_path);
+
+    while(!save.eof()){
+
+        save.getline(data,32);
+
+        switch(data[0]){
+
+            case 'F':
+                break;
+
+            case 'J':
+                sscanf(data,"J %d",pontuacao);
+                break;
+
+            case 'K':
+                sscanf(data,"K %d %d",xis,ipsilon);
+                j1->SetPosX(xis);
+                j1->SetPosY(ipsilon);
+                break;
+
+            case 'L':
+                sscanf(data,"L %d %d",xis,ipsilon);
+                j2->SetPosX(xis);
+                j2->SetPosY(ipsilon);
+                break;
+
+            case 'I':
+                sscanf(data,"I %d %d %d", xis, ipsilon, f);
+                switch(f){
+
+                    case 0:
+                        li->colaNoFinal(new Mumia(j1,j2,xis,ipsilon));
+                        break;
+
+                    case 1:
+                        break;
+
+                    case 2:
+                        break;
+
+                    case 3:
+                        break;
+
+                }
+                break;
+
+            case 'P':
+                sscanf(data,"P %d %d %d %d",xis,ipsilon,w,h);
+                lp->colaNoFinal(new Plataforma(xis,ipsilon,w,h));
+                break;
+
+        }
+
+    }
+
+    free(data);
+    save.close();
 
 }
