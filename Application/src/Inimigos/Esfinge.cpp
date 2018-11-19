25b8909f5a57fba6defd813c8ff729a2f2b5220b
Pedro Sokulski <pedro.vicente.a.s@gmail.com>
2018-11-19 11:33:13 -0200

    Update Esfinge.cpp

--- Application/src/Inimigos/Esfinge.cpp
+++ Application/src/Inimigos/Esfinge.cpp
@@ -2,9 +2,9 @@
 
 const int E_total_anims = 4;
 const int E_anim_parado = 0;
-const int E_anim_pulando = 1;
-const int E_anim_atacando = 2;
-const int E_anim_guardando = 3;
+const int E_anim_atacando = 1;
+const int E_anim_guardando = 2;
+const int E_anim_andando = 3;
 
 Esfinge::Esfinge(Jogador *j1, Jogador *j2, Lista<Projetil*>* projeteis, int x, int y):
 Inimigo(x, y, projeteis, E_total_anims){
@@ -24,9 +24,9 @@ Inimigo(x, y, projeteis, E_total_anims){
 
 	_range = ConstsPersonagens::M_distancia_deteccao * 4;
 	_range_ataque = 40;
-    _range_dist = 150;
 
 	_x0 = x;
+	_vida = 10;
 
 	_animador.SetTextureBox(&_tex_rect);
 	_animador.SetSprite(&_sp);
@@ -36,14 +36,58 @@ Inimigo(x, y, projeteis, E_total_anims){
 	_animador.SetPeriodo(0.1);
 
 	_animador.SetFrameCount(E_anim_parado, 1);
-	_animador.SetFrameCount(E_anim_pulando, 1);
+	_animador.SetFrameCount(E_anim_andando, 2);
 	_animador.SetFrameCount(E_anim_atacando, 2);
 	_animador.SetFrameCount(E_anim_guardando, 1);
     
 }
 
+Esfinge::~Esfinge(){}
+
+void Esfinge::Executar(float dt){
+
+	/****  Temporario ****/
+	if (_posicao.y + _sp.getGlobalBounds().height > Janela::altura) {
+		_posicao.y = 0;
+	}
+	/****  Temporario/ ****/
+
+	float dist_j1;
+	float dist_j2;
+	float mag_dist_j1;
+	float mag_dist_j2;
+	
+	if (_j1 != NULL) {
+		dist_j1 = _j1->GetPosicao().x - _posicao.x;
+		mag_dist_j1 = dist_j1 > 0? dist_j1: -dist_j1;
+	}
+	if (_j2 != NULL) {
+		dist_j2 = _j2->GetPosicao().x - _posicao.x;
+		mag_dist_j2 = dist_j2 > 0? dist_j2: -dist_j2;
+	}
+
+	if (!_esta_no_chao) {
+		_aceleracao.y = Fisica::G;
+	}
+	else {
+		_aceleracao.y = 0;
+	}
+	
+	switch(_estado){
+	case EstadoEsfinge::Esperando:
+		_animador.Play(E_anim_parado);
+		_animador.SetLoop(true);
+		if(!_esta_no_chao){
+			_estado = EstadoEsfinge::Pulando;
+			
+		
+		
+	}
+
+}
+	
 bool Esfinge::PodeMachucar(){
-	if(_estado == EstadoEsfinge::Esperando){
+	if(_estado == EstadoEsfinge::Guardando){
 		return true;
 	}
 	else{
